8d40f5ffdd7b9d000af85143e4fdfd1ee0707a71
brycebjork <bryce.j.bjork@gmail.com>
2017-01-24 12:21:46 -0700

    some more git functionality

--- updateOrCommit.bat
+++ updateOrCommit.bat
@@ -1,18 +1,17 @@
 @echo off
 call ./setvar gitremote git config --get remote.origin.url
-echo %gitremote%
-if %gitremote% == "https://github.com/brycebjork/Combinator_v9.git" (
+IF "%gitremote%" == "https://github.com/brycebjork/Combinator_v9.git" (
 	echo ------------------------------------------------
 	echo --                                            --
 	echo --     COMMITTING CHANGES TO MASTER           --
 	echo --                                            --
 	echo ------------------------------------------------
-    ::call ./autocommit
+	call ./autocommit
 ) ELSE (
 	echo ------------------------------------------------
 	echo --                                            --
 	echo --          UPDATING FROM MASTER              --
 	echo --                                            --
 	echo ------------------------------------------------
-    ::call ./update
+	call ./update
 )
